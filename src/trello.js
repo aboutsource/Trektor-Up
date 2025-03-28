408323dde29fa03875b9a8ce196ea18a6a20d227
Stefan Walluhn <stefan.walluhn@aboutsource.net>
2025-03-28 18:04:19 +0100

    hide button, needs to find better UI for update and delete

--- src/trello.js
+++ src/trello.js
@@ -1,21 +1,21 @@
 import { TogglGateway, TogglService } from "./toggl.js";
 
-const withErrorMessage = async function(t, fnc) {
+const withErrorMessage = async function (t, fnc) {
   try {
     await fnc(t);
   } catch (error) {
     t.alert({ message: error.message });
     throw error;
   }
-}
+};
 
-const stripStoryPointsAndTaskToken = function(name) {
+const stripStoryPointsAndTaskToken = function (name) {
   return name
     .replace(/^(\s*\(\d+\))?\s*/, "") // story points, e.g. (3)
     .replace(/\s*#\w+\s*$/, ""); // task token, e.g. #orga_5417
 };
 
-const extractTrackingData = async function(t) {
+const extractTrackingData = async function (t) {
   const card = await t.card("name", "labels", "idShort", "shortLink");
 
   const projectLabels = card.labels
@@ -34,13 +34,13 @@ const extractTrackingData = async function(t) {
     task: `${project}_${card.idShort}_${card.shortLink}`,
     description: stripStoryPointsAndTaskToken(card.name),
   };
-}
+};
 
-const setTrackingData = async function(t, tracking) {
+const setTrackingData = async function (t, tracking) {
   await t.set("card", "shared", "tracking", tracking);
-}
+};
 
-const track = async function(t) {
+const track = async function (t) {
   const token = await t.loadSecret("togglToken");
   if (token === null) {
     throw new Error("Toggl API Token not configured");
@@ -54,27 +54,27 @@ const track = async function(t) {
   t.alert({ message: `Started trekking #${tracking.task}` });
 };
 
-const updateTracking = async function(t) {
+const updateTracking = async function (t) {
   const tracking = await extractTrackingData(t);
   await setTrackingData(t, tracking);
 
   t.alert({ message: `Updated trekking #${tracking.task}` });
-}
+};
 
-const deleteTracking = async function(t) {
-  t.remove("card", "shared", "tracking")
+const deleteTracking = async function (t) {
+  t.remove("card", "shared", "tracking");
 
   t.alert({ message: "Deleted trekking" });
 };
 
 export default {
-  track: async function(t) {
+  track: async function (t) {
     await withErrorMessage(t, track);
   },
-  updateTracking: async function(t) {
+  updateTracking: async function (t) {
     await withErrorMessage(t, updateTracking);
   },
-  deleteTracking: async function(t) {
+  deleteTracking: async function (t) {
     await withErrorMessage(t, deleteTracking);
   },
 };
